dd9615ef2da100096eb1886af1f4f6e60ed80ce9
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-10 16:14:46 -0600

    general logic finished I thin, have to finish GetChar to see if it works first

--- finalExam/finalExam1.asm
+++ finalExam/finalExam1.asm
@@ -12,12 +12,16 @@
         move    $t1, $v0
         jal     GetChar
         move    $t2, $v0
-
+        li      $t3, 10
+        mult    $t1, $t1, $t3
+        add     $t0, $t0, $t1
+        mult    $t3, $t3, $t3
+        mult    $t2, $t2, $t3
+        add     $t0, $t0, $t2 
         # make chars digits and sum
-
-        
-
-
+        move    $a0, $t0
+        li      $v0, 1
+        syscall
         # print digits
 
     li  $v0, 10

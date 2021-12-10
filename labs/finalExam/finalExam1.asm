dc679cb02b65ab4a901bb40c2ef3af9c4277f9c7
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-10 05:23:17 -0600

    added a little meat to skeleton, gonna take a break and finish main. GetChar is last task to tackle

--- labs/finalExam/finalExam1.asm
+++ labs/finalExam/finalExam1.asm
@@ -5,11 +5,25 @@
 .text
 .globl main
     main:
-        # take in chars sequentially. 
+        # take in chars sequentially and save
+        jal     GetChar
+        move    $t0, $v0
+        jal     GetChar
+        move    $t1, $v0
+        jal     GetChar
+        move    $t2, $v0
 
         # make chars digits and sum
+
+        
+
 
         # print digits
 
     li  $v0, 10
     syscall
+
+
+    GetChar:
+
+        jr  $ra

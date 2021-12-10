397fa32ffac29913fd056cef7ec324a43c0ed973
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-10 05:19:05 -0600

    added skeleton

--- labs/finalExam/finalExam1.asm
+++ labs/finalExam/finalExam1.asm
@@ -1,9 +1,15 @@
+# program to take 3 character digits 
+
 .data
 
 .text
 .globl main
-
     main:
+        # take in chars sequentially. 
+
+        # make chars digits and sum
+
+        # print digits
 
     li  $v0, 10
     syscall

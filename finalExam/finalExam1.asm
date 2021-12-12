977a8c2c0bd17319248d44a39ff6c8559636ec2e
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-11 22:16:46 -0600

    changed mults to muls

--- finalExam/finalExam1.asm
+++ finalExam/finalExam1.asm
@@ -13,10 +13,10 @@
         jal     GetChar
         move    $t2, $v0
         li      $t3, 10
-        mult    $t1, $t1, $t3
+        mul    $t1, $t1, $t3
         add     $t0, $t0, $t1
-        mult    $t3, $t3, $t3
-        mult    $t2, $t2, $t3
+        mul    $t3, $t3, $t3
+        mul    $t2, $t2, $t3
         add     $t0, $t0, $t2 
         # make chars digits and sum
         move    $a0, $t0

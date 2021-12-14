f50be2fe9a8425d1cec145920e6cbea2c1d5a6a6
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-14 16:43:42 -0600

    completed main I think

--- finalExam/finalExam2.asm
+++ finalExam/finalExam2.asm
@@ -1,8 +1,16 @@
 .text
 .globl main
     main:
-        # enable interrupts 
+        # enable interrupts on receiver 
+        li		$t0, 2
+        sw		$t0, 0xffff0000
 
-        # infiniloop
+        # enable all interrupts
+        li		$t0, 0xff01
+        mtc0	$t0, $12 
+
+        # infiniloopa
+        here: j here
 
     .ktext
+        

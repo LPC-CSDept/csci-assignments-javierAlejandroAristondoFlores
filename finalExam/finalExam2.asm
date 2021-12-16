9daa73025a153d2a46886765758c6eaee3cc8491
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-16 02:56:48 -0600

    lost my train of thought, going to re do kernel tect

--- finalExam/finalExam2.asm
+++ finalExam/finalExam2.asm
@@ -1,3 +1,6 @@
+.data
+    KERNEL_TEMP_BUFFER  .space  8
+
 .text
 .globl main
     main:
@@ -5,7 +8,7 @@
         li		$t0, 2
         sw		$t0, 0xffff0000
 
-        # enable all interrupts
+        # enable all interrupts on co-processor zero
         li		$t0, 0xff01
         mtc0	$t0, $12 
 
@@ -13,16 +16,4 @@
         here: j here
 
     .ktext
-        # read data
-        lw      $k0, 0xffff0004
-
-        # check if input was q
-        bneq    $k0, 'q', notEqual
-
-        # yes
-        li      
-
-        # no
-        notEqual:
-
-
+        

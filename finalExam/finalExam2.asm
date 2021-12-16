8a835a484db56730abef769a6b074a6be269d973
javierAlejandroAristondoFlores <95454903+javierAlejandroAristondoFlores@users.noreply.github.com>
2021-12-16 16:22:11 -0600

    got first bit done of ktecxt

--- finalExam/finalExam2.asm
+++ finalExam/finalExam2.asm
@@ -7,19 +7,21 @@
         # enable interrupts on receiver 
         li		$t0, 2
         sw		$t0, 0xffff0000
-
         # enable all interrupts on co-processor zero
         li		$t0, 0xff01
         mtc0	$t0, $12 
-
         # infiniloop
         here: j here
 
-    .ktext 0x80000180
-        # check if exception was an interrupt
-        
-        # check if q
+.ktext 0x80000180
+    # check if exception was an interrupt
+    mfc0	$k0, $13
+    andi	$k0, $k0, 0x7c
+    bnez	$k0, end
+    # check if q
 
-        # yes
+    # yes
 
-        # no
+    # no
+
+    end:
